35ae56fb199deeb67712b1d89a2143ef6e5f1a3a
agotetyx <70243580+agotetyx@users.noreply.github.com>
2020-11-21 16:14:04 -0500

    completed

--- Pivot.gd
+++ Pivot.gd
@@ -5,8 +5,10 @@ export var speed = 15
 export var zoom_speed = 1.5
 export var zoom_min = 5
 export var zoom_max = 15
+onready var scene = load("res://Assets/scene.gltf")
 
 func _physics_process(delta):
+	#look_at(scene, )
 	rotation_degrees.y += delta*speed
 	$Camera.translation.z += delta*zoom_speed
 	if $Camera.translation.z > zoom_max or $Camera.translation.z < zoom_min:
